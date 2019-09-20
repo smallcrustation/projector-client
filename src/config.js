8c444bb4b732f559dd7c8a9f04c3bae7fae887b5
krill <lpmaze@gmail.com>
2019-09-20 16:37:24 -0400

    changed cors to ()

--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
 export default{
-  API_ENDPOINT: 'http://localhost:8000/api',
+  API_ENDPOINT: 'http://localhost:8000/api', // 'https://ancient-mesa-23863.herokuapp.com/api',
   TOKEN_KEY: 'dev-client-auth-token'
+  // API_ENDPOINT: 'https://ancient-mesa-23863.herokuapp.com/api',
+  // TOKEN_KEY: '2e896ea7-7f61-4dc5-b459-957268e27122'
 }
