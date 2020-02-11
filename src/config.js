57c19249b374d003c033baba69170595c6354158
krill <lpmaze@gmail.com>
2020-02-11 17:50:09 -0500

    jerry rigged API_ENDPOINT

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default{
-  API_ENDPOINT: 'http://localhost:8000/api',
-  TOKEN_KEY: 'dev-client-auth-token'
-  // API_ENDPOINT: 'https://ancient-mesa-23863.herokuapp.com/api',
-  // TOKEN_KEY: '2e896ea7-7f61-4dc5-b459-957268e27122'
+  // API_ENDPOINT: 'http://localhost:8000/api',
+  // TOKEN_KEY: 'dev-client-auth-token'
+  API_ENDPOINT: 'https://ancient-mesa-23863.herokuapp.com/api',
+  TOKEN_KEY: '2e896ea7-7f61-4dc5-b459-957268e27122'
 }
